3078ba0b5688b1e095cee486ede8f3f449449290
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2024-01-25 16:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function delayedHello(){
+	 return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve('Hello, world!');
+          }, 1000); // 1 second delay
+        });
+}
+ const outputElement = document.getElementById('output');
+delayedHello()
+        .then((message) => {
+          outputElement.textContent = message;
+        })
+        .catch((error) => {
+          console.error('Promise rejected:', error);
+        });
